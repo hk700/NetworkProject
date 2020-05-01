f07403024bf726151eceee76265d7ae654026a12
Omar Atieh <43860243+atiehomar97@users.noreply.github.com>
2020-05-01 13:30:47 -0400

    read

--- dijkstras.py
+++ dijkstras.py
@@ -128,4 +128,4 @@ class Graph:
 
 if __name__== "__main__":
     graph = Graph([("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),("e", "f", 9)])
-    graph.test("a","d")
+    graph.test("a","e")
