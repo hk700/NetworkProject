abda1877e5168e6a57f9c06ed101ad01ce9608b1
jal501 <jal501@scarletmail.rutgers.edu>
2020-05-07 16:36:02 -0400

    client

--- udpclient.py
+++ udpclient.py
@@ -12,7 +12,7 @@ class udpclient():
 
 #Initializes a new client and starts the ping
 if __name__ == '__main__':
-       # udp_client = udpclient(id=101, ip='192.168.1.1', gateway=('192.168.1.2',8881), port=8880)
-       # ping(udp_client, c=100, dst=103)
-        udp_client = udpclient(s1.id, ip=s1.ip, gateway=(r1.ip,s1.port), port=8880)
-        ping(udp_client, c=100, dst=d2.id)
+        udp_client = udpclient(id=101, ip='192.168.1.1', gateway=('192.168.1.2',8881), port=8880)
+        ping(udp_client, c=100, dst=102)
+        #udp_client = udpclient(s1.id, ip=s1.ip, gateway=(r1.ip,s1.port), port=8880)
+        #ping(udp_client, c=100, dst=d2.id)
